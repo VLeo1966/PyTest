a4ef89dafe951081ce8883fdac9d4bb4cee59b01
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-10-15 19:19:35 +0300

    did dz

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import pytest
 from main import check
 from main import isPalindrome
+from main import sort_list
+from main import init_db, add_user, get_user
 
 def test_check():
     assert check(6) == True
@@ -35,6 +37,23 @@ def test_isPalindrome1():
 def test_is_Palindrome(test_input, expected):
     assert isPalindrome(test_input) == expected
 
+
+def test_sort1():
+    assert sort_list([5, 6, 3, 1, 2]) == [1, 2, 3, 5, 6]
+    assert sort_list([-1, 3, 0, -2, 2]) == [-2, -1, 0, 2, 3]
+
+
+@pytest.fixture
+def db_conn():
+    conn = init_db()
+    yield conn
+    conn.close()
+
+
+def test_add_or_get_user(db_conn):
+    add_user(db_conn, "Sasha", 30)
+    user = get_user(db_conn, "Sasha")
+    assert user == (1, "Sasha", 30)
 
 if __name__ == "__main__":
     pytest.main()
